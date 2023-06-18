d164e0273efcbddf905aeaab0b93f06b6ea62043
sijan-surf <64152806+sijan-surf@users.noreply.github.com>
2023-06-18 01:07:03 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,32 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import RootNavigator from './routes/rootStack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.staffView}>STAFF DIRECTORY</Text>
-      <Text>John Smith</Text>
-      <Text>Sue White</Text>
-      <Text>Bob O' Bits</Text>
-      <Text>Mary Blue</Text>
-      <Text>Mick Green</Text>
-      
-      <StatusBar style="auto" />
-    </View>
+    
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  staffView: {
-    fontFamily: 'Arial',
-    fontSize: 26,
-    fontWeight: 'bold',
-    justifyContent: 'space-between',
-  }
-});
