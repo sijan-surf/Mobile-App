aa7df95a6717b061723fac9cecb92380037c41eb
sijan-surf <64152806+sijan-surf@users.noreply.github.com>
2023-06-18 01:05:38 +0000

    Add files via upload

--- routes/rootStack.js
+++ routes/rootStack.js
@@ -1,16 +1,42 @@
-import React from "react";
-import {creteStackNavigator} from '@react-navigation/stack';
-import {Ionicons} from '@expo/vector-icons';
-const stack = createStackNavigator();
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Ionicons } from '@expo/vector-icons';
+const Stack = createStackNavigator();
 
 import Post from '../components/Post';
 import Get from '../components/Get';
-import Detail from '../components/Detail';
+import Detail from '../components/Detail'
 
-const rootStack = () {
+const rootStack = () => {
     return (
-        <Stack.Navigator>
-        
-        
-    )
-}
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: 'white',
+                },
+                headerTintColor: 'black',
+            }}>
+            <Stack.Screen
+                name="Get"
+                component={Get}
+                options={({ navigation, route }) => ({
+                    title: 'STAFF DIRECTORY',
+                    headerTitleAlign: 'center',
+                    headerRight: () => (
+                        <Ionicons
+                            name={'add-outline'}
+                            size={45}
+                            color={'black'}
+                            style={{ marginRight: 15 }}
+                            onPress={() => navigation.navigate('Post')}
+                        />
+                    ),
+                })}
+            />
+            <Stack.Screen name="Post" component={Post} />
+            <Stack.Screen name="Detail" component={Detail} />
+        </Stack.Navigator>
+    );
+};
+
+export default rootStack;
